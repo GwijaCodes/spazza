21a07e1a7a02b1ebb41e5a4534cb1a9a7af575a5
GwijaCodes <133336462+GwijaCodes@users.noreply.github.com>
2023-07-28 19:24:07 +0200

    Updated settings

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
+//cache
 self.addEventListener('install', (e)=>{
     e.waitUntil(
         caches.open("static").then(cache => {
             return cache.addAll([
+            "https://fonts.cdnfonts.com/css/romero",
+            "https://fonts.cdnfonts.com/css/roboto",
+            "https://unpkg.com/boxicons@2.1.4/dist/boxicons.js",
             "./images",
-            "./"
+            "./",
+            "./src/fallback.html",
         ])
         })
     );
 })
 
+//offline
 self.addEventListener("fetch", e=>{
     e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch(e.request);
-        })
+        }).catch(() => caches.match('./src/fallback.html'))
     )
 });
+
+
 
