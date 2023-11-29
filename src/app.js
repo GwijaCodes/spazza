d6af4c50e229522759dfc2e289622ba784513928
GwijaCodes <133336462+GwijaCodes@users.noreply.github.com>
2023-11-29 17:02:04 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
 //dati array
 const settimana = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
 const mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
-const raccolta = ["Umido", "Secco", "Umido Vetro", "Plastica", "Carta", "Umido Metallo", "Buon Sabato"]
+const raccolta = ["Umido", "Secco", "Umido e Vetro", "Plastica", "Carta", "Umido e Metallo", "Buon Sabato"]
 const colori = ['images/rosso.jpg', 'images/grigio.jpeg', 'images/verde.jpg', 'images/giallo.jpg', 'images/blu.jpg', 'images/verde.jpg', 'images/sabato.jpg'];
 
 //mostra data
 let date = new Date();
-let weekday = settimana[date.getDay()];
+let dayN = date.getDay();
+let weekday = settimana[dayN];
 let month = mesi[date.getMonth()];
 let day = date.getDate();
 let oggi = document.querySelector('#oggi').innerHTML = `${weekday} ${day} ${month}`
 
 //mostra cosa buttare oggi e domani
-let spazza = raccolta[date.getDay()];
+let spazza = raccolta[dayN];
 let spazzaText = document.querySelector('#spazzaText').innerHTML = spazza;
 let domani = document.querySelector('#spazzaDomani');
+//trashbin
+const trashbin = document.querySelector('.trashbin');
+trashbin.style.backgroundPositionX = 1365 - (195 * dayN) + 'px';
+
+console.log(trashbin.style.backgroundPositionX)
 
 //loop da sabato a domenica
-if (date.getDay() >= raccolta.length - 1){
+if (dayN >= raccolta.length - 1){
     domani.innerHTML = raccolta[0];
 } else {
-    domani.innerHTML = raccolta[date.getDay() + 1];
+    domani.innerHTML = raccolta[dayN + 1];
 }
 
 //calendario espandibile
